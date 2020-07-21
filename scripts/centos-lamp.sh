aed5dddfa1c0144fd2b7924635cb513bee3aaa4c
kenny norris <kenny.norris@outlook.com>
2020-07-21 17:00:44 +0100

    adding new files

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#updating centos with any patches 
+yum update -y --exclude=kernel
+
+# installing tools
+yum install -y nano git unzip screen 
+
+# installing tools 
+yum install -y httpd httpd-devel httpd-tools 
+chkconfig --add httpd 
+chkconfig  httpd on 
+service httpd stop 
+
+rm -rf /var/www/html 
+ln -s /vagrant /var/www/html 
+
+service httpd start
+
+# install php 
+yum install -y php php-cli php-common php-devel php-mysql 
+
+#mysql 
+yum install -y mysql mysql-server mysql-devel 
+chkconfig --all mysqld
+chkconfig mysqld on 
+
+service mysqld start 
+
+mysql -u root -e "SHOW DATABASES";
+
+# download starter content 
+
+
+
+service httpd restart 
+
